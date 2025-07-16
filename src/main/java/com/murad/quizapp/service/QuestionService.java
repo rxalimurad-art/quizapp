c264093bbbdc7e4f4b25c85fc2b683d4a660b0c4
alimurad <amurad@i2cinc.com>
2025-07-16 11:36:08 +0500

    -- fetch

---
+++ src/main/java/com/murad/quizapp/service/QuestionService.java
@@ -0,0 +1,20 @@
+package com.murad.quizapp.service;
+
+
+import com.murad.quizapp.dao.QuestionDao;
+import com.murad.quizapp.model.Question;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class QuestionService {
+    @Autowired
+    QuestionDao questionDao;
+
+    public List<Question> getAllQuestions() {
+        questionDao.findAll().forEach(question -> System.out.println(question.getQuestionTitle()));
+        return questionDao.findAll();
+    }
+}
